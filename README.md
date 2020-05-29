2fc2271f75c10707302199eb3123cbbf2f949693
glimmerald <glimmerald@gmail.com>
2020-05-29 12:09:31 +0300

    refactoring all items

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # test-for-AmberLight
+Тестовое задание по созданию Лендинга по данному макету:
+https://www.figma.com/file/Xf0Oef8SzokB70snCukmNlI8/mamon.pro?node-id=0%3A1.
+Выполнен:
+1. Адаптив до 320px.
+2. Форма вынесена с помощью JS как popup.
--- app/js/main.js
+++ app/js/main.js
@@ -1,41 +1,11 @@
-let popup = document.getElementById('mypopup'),
+let popup = document.getElementById('myPopup'),
     popupToggle = document.getElementById('myBtn'),
     popupClose = document.querySelector('.close');
 
 popupToggle.onclick = function() {
-    popup.style.display = "block";
+    popup.classList.toggle("show");
 };
 
 popupClose.onclick = function() {
-    popup.style.display = "none";
+    popup.classList.toggle("show");
 };
-
-window.onclick = function(e) {
-    if (e.target == popup) {
-        popup.style.display = "none";
-    }
-}
-
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.menu-list');
-    const navLinks = document.querySelectorAll('.menu-list li')
-
-    burger.addEventListener('click', () => {
-        // Toggle Nav
-        nav.classList.toggle('nav-active');
-
-        // Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = ''
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.4}s`;
-            }
-        });
-        //Burger Animation
-        burger.classList.toggle('toggle')
-    });
-}
-
-navSlide();
